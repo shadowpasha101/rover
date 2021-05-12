6c29f5f59ae3609585fccc9be1a4e1d0e661c2a5
shadowpasha101 <35360258+shadowpasha101@users.noreply.github.com>
2021-05-12 23:58:12 +1000

    Create Motor_lib.h
    Motor lib header

---
+++ Motor_lib.h
@@ -0,0 +1,23 @@
+#ifndef Motor_lib_H
+#define Motor_lib_h
+#include <Arduino.h>
+
+void Go_Forward();
+
+void Go_turn_45_left();
+
+void Go_turn_45_right();
+
+void motor_stop();
+
+void Go_turn_180();
+
+void forwardmotor();
+
+void reversemotor();
+
+void stopmotor();
+
+
+ 
+#endif
